22814c09660925b9ac9a784eac70723ab9b4c452
kunal pardeshi <pardeshi.kunal.kishor@gmail.com>
2023-10-08 17:51:02 +0530

    esling & vite setup

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { createRoot } from "react-dom";
+
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
+  ]);
+};
+
+const container = document.getElementById("root");
+//ReactDOM.render(<App/>, container);
+const root = createRoot(container);
+root.render(React.createElement(App));
